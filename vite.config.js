c385d55e5c0b49e4035e81ecdbae4eb91dac8a8f
Vaibhav Giradkar <giradkarvaibhav2@gmail.com>
2026-01-21 20:05:25 +0530

    Fix Vite base config error

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || "/",
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
-        base: process.env.VITE_BASE_PATH || "/medPulse"
       },
     }),
   ],
