c0a9652b542860bf5cb18da5a5a8274c2350fce8
Vaibhav Giradkar <giradkarvaibhav2@gmail.com>
2026-01-21 20:00:04 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
+        base: process.env.VITE_BASE_PATH || "/medPulse"
       },
     }),
   ],
