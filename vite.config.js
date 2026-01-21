b752faf7acfcb26d02e37732ce8bfa73cee5685f
Vaibhav Giradkar <giradkarvaibhav2@gmail.com>
2026-01-21 20:23:04 +0530

    Remove unstable react compiler plugin

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || "/",
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  base: "/",
+  plugins: [react()],
 })
